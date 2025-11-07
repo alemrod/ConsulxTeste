230d0fe1ed25e81c244b6a03b4ae419cdf2f1972
Alexandre Mori Rodrigues <139145007+alemrod@users.noreply.github.com>
2025-11-07 17:12:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import numpy as np
 import streamlit as st
 import plotly.express as px
 import plotly.graph_objects as go
-from prophet import Prophet
+from statsmodels.tsa.arima.model import ARIMA
 from fpdf import FPDF
 
 # ------------------------------
